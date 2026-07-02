74141fd35f8261686d6dddc1673e44acbbe3ea2e
Joshua <>
2026-07-02 22:19:38 +1000

    Added basic directory and filename pattern filtering.

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ I wrote this script as a result of other available solutions relying on
 Python's os.walk() and hence not producing an ordered output. The purpose
 was to have a way to create a playlist of albums or audiobooks that 
 preserved the file order for each subdirectory and would optionally 
-accept a list of input folders to use as well.
+accept a list of input folders to use as well (whitelist behaviour 
+rather than blacklist).
 
-Related scripts (better suited to randomized music playlists):
+Related scripts that served as inspiration (better suited to randomized 
+music playlists):
 
 - https://github.com/bellerofonte/m3u-creator
 - https://pypi.org/project/m3u-maker/
@@ -35,6 +37,10 @@ Optional arguments:
 | --- | --- | --- |
 | -t, --top-directory | /path/to/your/music | Set the top level directory for the search. Default is the current working directory. |
 | -f, --filetypes | .mp3 .flac .ogg | Specify what file types to search for; currently this matches only on file extension and does not inspect the file headers. Default is some common audio formats. |
+| --file-exclude | "Track Name" "Promo" | This restricts the contents of the playlist to filenames that do _not_ any of the keywords or phrases specified. Case-insensitive. Overrides inclusion list. |
+| --file-include | "Track Name" "Promo" | This restricts the contents of the playlist to filenames that _must_ include one of the keywords or phrases specified. Case-insensitive. |
+| --directory-exclude | "Artist Name" "Album Name" | This restricts the contents of the playlist to directory paths that do _not_ any of the keywords or phrases specified. This works best if you have your library in directories by artist, album or audiobook. Case-insensitive. Overrides inclusion list. |
+| --directory-include | "Artist Name" "Album Name" | This restricts the contents of the playlist to directory paths that _must_ include one of the keywords or phrases specified. This works best if you have your library in directories by artist, album or audiobook. Case-insensitive. |
 | -s, --sort-method | ascii | Specify which sort method to use on the playlist. The default behaviour is 'ascii', which tries to match the default Unix ls and Windows Explorer (pre Windows 10) sort order. If the `natsort` package is installed, you can also specify 'natural' as the sort order. 'unsorted' uses the default behaviour of `os.walk`. |
 | -r, --relative | | Boolean flag to force the use of relative paths in the playlist (paths will be relative to the starting top level directory). Default behaviour is to use absolute paths. |
 | -e, --extm3u | | Boolean flag to request the inclusion of EXTM3U and EXTINF tags in the playlist. Requires the `music-tag` package. |
--- m3u-creator.py
+++ m3u-creator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
-#
+
+"""This script creates an ordered M3U playlist from a source directory tree."""
+
 #  m3u-creator.py
-#  
+#
 #  Copyright 2026 Joshua White
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  See README.MD or https://github.com/Australis86/m3u-creator 
+#
+#  See README.MD or https://github.com/Australis86/m3u-creator
 #  for documentation
 
 
@@ -37,9 +39,17 @@ except ImportError:
     music_tag = None
 
 
-def searchDirectoryTree(init_path, filetypes, sortmethod='ascii'):
-    '''Search the directory tree for matching file types, sorting 
-    by the selected method. Returns a list of absolute file paths.'''
+def searchDirectoryTree(init_path, filtering_dict, sortmethod='ascii'):
+    '''Search the directory tree for matching file types, sorting
+    by the selected method. Returns a list of absolute file paths.
+
+    filtering_dict = {
+        'filetypes': list of file extensions to search for (required),
+        'directory-include': list of keywords to match in paths (optional),
+        'directory-exclude': list of keywords to exclude in paths (optional),
+        'file-include': list of keywords to match in filenames (optional),
+        'file-exclude': list of keywords to exclude in filenames (optional),
+    }'''
 
     ec = 0 # Exit code
 
@@ -47,14 +57,14 @@ def searchDirectoryTree(init_path, filetypes, sortmethod='ascii'):
         print(os.linesep,"ERROR: natsort package missing -- natural sorting not available, falling back to ascii",os.linesep)
         sortmethod = 'ascii'
         ec = 2
-        
+
     print(f"Searching {init_path}...")
     results = []
 
     for path, subdirs, files in os.walk(init_path):
-        # os.walk yields at each step, so by sorting here we can 
+        # os.walk yields at each step, so by sorting here we can
         # influence the order in which the directory tree is recursed
-        
+
         # Sort the subdirectories and folders
         if sortmethod == 'alphabetical':
             subdirs.sort()
@@ -64,32 +74,56 @@ def searchDirectoryTree(init_path, filetypes, sortmethod='ascii'):
             subdirs.sort(key=naturalsort)
             files.sort(key=naturalsort)
 
-        # If no sorting is conducted, os.walk will fall back to its 
+        # If no sorting is conducted, os.walk will fall back to its
         # default behaviour as per listdir
-        
-        # Filter files based on filetypes - use the last extension to
-        # avoid issues where filenames contain a period
-        filtered_files = [ os.path.join(path, f) for f in files if os.path.splitext(f)[-1] in filetypes ]
+
+        # If directory inclusion is enabled, check that we're in a relevant subdirectory
+
+        # Filter paths:
+        #   1. if include list is present, check the path and disregard
+        #      present directory if there are no matches
+        #   2. if exclude list is present, check the path and disregard
+        #      present directory if there are any matches
+        inclusion_test = filtering_dict['directory-include'] and not any(w.lower() in path.lower() for w in filtering_dict['directory-include'])
+        exclusion_test = filtering_dict['directory-exclude'] and any(w.lower() in path.lower() in path for w in filtering_dict['directory-exclude'])
+        if inclusion_test or exclusion_test:
+            continue
+
+        # Filter files:
+        #   1. filetypes - use the last extension to avoid issues where
+        #      filenames contain a period
+        #   2. if include list is present, check the filename and disregard
+        #      files where there are no matches
+        #   3. if exclude list is present, check the filename and disregard
+        #      files where there are any matches
+        filtered_files = files
+
+        if filtering_dict['file-include']:
+            filtered_files = [ f for f in filtered_files if any(w.lower() in f.lower() for w in filtering_dict['file-include']) ]
+        if filtering_dict['file-exclude']:
+            filtered_files = [ f for f in filtered_files if any(w.lower() not in f.lower() for w in filtering_dict['file-exclude']) ]
+
+        filtered_files = [ os.path.join(path, f) for f in filtered_files if os.path.splitext(f)[-1] in filtering_dict['filetypes'] ]
         found_files = len(filtered_files)
         if found_files > 0:
             print(f"{found_files} file(s) in {path}")
-            
+
         results.extend(filtered_files)
-        
+
     return (results, ec)
 
 
 def buildPlaylist(filelist, playlist, init_path, relpaths=False, extended_m3u=False):
     '''Create the M3U playlist. Include EXTM3U and EXTINF if requested.
     EXTINF tags are of format: #EXTINF:LENGTH, ARTIST - TRACK'''
-    
+
     ec = 0 # Exit code
-    
+
     if extended_m3u and music_tag is None:
         print(os.linesep,"ERROR: music-tag package missing -- extended M3U is not available, falling back to basic M3U",os.linesep)
         extended_m3u = False
         ec = 2
-    
+
     with open(playlist, 'w', encoding='utf-8') as f:
         if extended_m3u:
             f.write("#EXTM3U\n")
@@ -119,30 +153,45 @@ def buildPlaylist(filelist, playlist, init_path, relpaths=False, extended_m3u=Fa
     return ec
 
 
-def main(args):
-    return 0
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Create an ordered M3U playlist by scanning a directory tree for matching file types."
     )
-    
+
     parser.add_argument("playlist",
         help="Filename for your playlist; e.g. music.m3u")
     parser.add_argument("-t","--top-directory",
         default=os.getcwd(),
         help="Top-level directory to search within for files to add to playlist (default: current working directory)")
+
+    # File filtering
     parser.add_argument(
         "-f","--filetypes",
         nargs="*",
-        default=[".mp3",".flac",".m4b",".wav",".aac"],
+        default=[".mp3",".flac",".ogg",".wma",".wav",".webm",".aac",".m4a",".m4b"],
         help="File extensions to include as a space-separated list")
     parser.add_argument(
-        "-s","--sort-method",
-        default="ascii",
-        choices=["ascii","natural","unsorted"],
-        help="Method used to sort the playlist (default: ascii)")
+        "--file-exclude",
+        nargs="*",
+        default=None,
+        help='Space-separated words or phrases to filter on; any filenames containing any of these entries will be excluded. Example list: "Track 01" "Track 02" "Promo" ')
+    parser.add_argument(
+        "--file-include",
+        nargs="*",
+        default=None,
+        help='Space-separated words or phrases to filter on; any filenames NOT containing one of these entries will be excluded. Example list: "Track 01" "Track 02" "Promo" ')
+    parser.add_argument(
+        "--directory-exclude",
+        nargs="*",
+        default=None,
+        help='Space-separated words or phrases to filter on; any directories containing any of these entries will be excluded. Example list: "Artist 1" "Artist 2" "Album" ')
+    parser.add_argument(
+        "--directory-include",
+        nargs="*",
+        default=None,
+        help='Space-separated words or phrases to filter on; any directories NOT containing one of these entries will be excluded. Example list : "Artist 1" "Artist 2" "Album" ')
+
+    # Playlist structure
     parser.add_argument(
         "-e","--extm3u",
         action="store_true",
@@ -153,9 +202,23 @@ if __name__ == '__main__':
         action="store_true",
         default=False,
         help="Use relative paths in the playlist rather than absolute")
+    parser.add_argument(
+        "-s","--sort-method",
+        default="ascii",
+        choices=["ascii","natural","unsorted"],
+        help="Method used to sort the playlist (default: ascii)")
 
     args = parser.parse_args()
 
-    (mediafiles, ec1) = searchDirectoryTree(args.top_directory, args.filetypes, args.sort_method)
+    filter_dict = {
+        'filetypes':args.filetypes,
+        'directory-include':args.directory_include,
+        'directory-exclude':args.directory_exclude,
+        'file-include':args.file_include,
+        'file-exclude':args.file_exclude
+    }
+
+    (mediafiles, ec1) = searchDirectoryTree(args.top_directory, filter_dict, args.sort_method)
+    # pylint: disable-next=C0103
     ec2 = buildPlaylist(mediafiles, args.playlist, args.top_directory, args.relative, args.extm3u)
-    exit(ec1 or ec2)
+    sys.exit(ec1 or ec2)
